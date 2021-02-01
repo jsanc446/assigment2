362ef7f8c55258624479b8750b0e4b67fa64aa81
jsanc446 <78328243+jsanc446@users.noreply.github.com>
2021-01-31 23:00:11 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -6,7 +6,7 @@ class Assignment2:
         
     def sayWelcome(self,name):
         self.name=name
-        print(f"Welcome to the assignment,{name}! Haven't seen you for {self.age} years!")
+        print(f"Welcome to the assignment, {name}!  Haven't seen you for {self.age} years!")
         
     def doubleList(self, theList):
         list=theList
